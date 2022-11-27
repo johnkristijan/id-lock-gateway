450e2a1cf41f67a7da7e43746a5b591c1d5da323
John K Hermann <john.hermann@webstep.no>
2022-11-27 21:56:19 +0100

    beta

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.NODE_PORT
 
-function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms)
-    })
-}
+const port = process.env.NODE_PORT
+const BASE = process.env.GATEWAY_BASE || 'https://'
+const HOST = process.env.GATEWAY_HOST || '192.168.0.250'
 
 app.get('/', (req, res) => {
     res.send('Server running.')
@@ -16,16 +13,18 @@ app.get('/', (req, res) => {
 app.post('/changepin/:leieobjekt/:six_digit_pin', async (req, res) => {
     const leieobjekt = req.params.leieobjekt
     const six_digit_pin = req.params.six_digit_pin
+    console.info(`>>> pin change request received; leieobjekt=${leieobjekt} | pin=${six_digit_pin}`)
+
     if (!leieobjekt) {
-        res.send('leieobjekt missing')
+        console.info(`>>> leieobjekt missing`)
+        res.status(400).send('leieobjekt missing')
         return
     }
     if (!six_digit_pin) {
-        res.send('six_digit_pin missing')
+        console.info(`>>> six_digit_pin missing`)
+        res.status(400).send('six_digit_pin missing')
         return
     }
-
-    // validate the pin!
 
     let door_lock_id = undefined
     if (leieobjekt === 'festsal' || leieobjekt === 'selskapslokale') {
@@ -35,50 +34,30 @@ app.post('/changepin/:leieobjekt/:six_digit_pin', async (req, res) => {
     } else if (leieobjekt === 'hybel2') {
         door_lock_id = 'ID_LOCK_2'
     } else {
-        res.send('leieobjekt undefined')
+        console.info(`>>> door_lock_id undefined`)
+        res.status(400).send('door_lock_id undefined')
         return
     }
 
-    const BASE = process.env.GATEWAY_BASE || 'https://'
-    const HOST = process.env.GATEWAY_HOST || '192.168.0.250'
-    let url = `${BASE}${HOST}/set_pin/user/1/pin/${six_digit_pin}`
-    const options = {}
-    const response = await fetch(url, options)
-    if (response && response.ok) {
-        await sleep(5000)
-        url = `${BASE}${HOST}/get_pin/user/1`
-        const verification = await fetch(url, options) // duration ~5 seconds
-        if (verification && verification.ok) {
-            try {
-                const verifiedPin = await verification.json()
-                if (six_digit_pin == verifiedPin) {
-                    res.send('OK: PIN changed - lock has confirmed new PIN: ' + six_digit_pin)
-                    return
-                } else {
-                    console.log('six_digit_pin :>> ', six_digit_pin)
-                    console.info(typeof six_digit_pin)
-                    console.log('verifiedPin :>> ', verifiedPin)
-                    console.info(typeof verifiedPin)
-                    res.status(400).send(`Could not verify new pin from lock ${door_lock_id}`)
-                    return
-                }
-            } catch (err) {
-                res.status(400).send(
-                    `Verification failed - user error on lock ${door_lock_id} due to ${err.message}`
-                )
-                return
-            }
+    if (door_lock_id === 'ID_LOCK_0') {
+        const URL = `${BASE}${HOST}/set/1/${six_digit_pin}`
+        const RESPONSE = await fetch(URL)
+        if (RESPONSE && RESPONSE.ok && RESPONSE.status == '200') {
+            console.info(`>>> pin change request successful; leieobjekt=${leieobjekt} | pin=${six_digit_pin}`)
+            res.send('OK')
         } else {
-            res.status(400).send(`Unsuccessful verification from lock ${door_lock_id}`)
+            console.info(`>>> pin change request failed; leieobjekt=${leieobjekt} | pin=${six_digit_pin}`)
+            res.status(400).send('Not OK')
         }
+        return
     } else {
-        res.status(400).send(`Unsuccessful response from lock ${door_lock_id}`)
+        console.info(`>>> door_lock_id ${door_lock_id} is not supported yet`)
+        res.status(400).send(`>>> door_lock_id ${door_lock_id} is not supported yet`)
         return
     }
-
-    res.send('changepin OK')
 })
 
 app.listen(port, () => {
-    console.log(`gateway receiver app running on port ${port}`)
+    console.log(`lock gateway app running on port ${port}`)
 })
+
